41293c31a7c401a6d96d037e6a0fa15e64c25ab4
MicheleFumi <michele.fumi96@gmail.com>
2024-10-22 14:15:57 +0200

    add info method

--- main.js
+++ main.js
@@ -22,3 +22,31 @@ cicla nell'array usando un forEach
 cicla nell'array usando un for
 crea una nuova array che contenga solo il nome dei veicoli
 crea una nuova array con solo i veicoli elettriche */
+
+// creo la classe con le proprità
+class Auto{
+    brand;
+    year;
+    color;
+    doors;
+    fuel;
+    constructor(brand, year, color, doors, fuel){
+
+        this.brand = brand
+        this.year = year
+        this.color = color
+        this.doors = doors
+        this.fuel = fuel
+       
+
+    }
+ // creo una funzione per loggare le info di una auto
+  informazioni(){
+    return this.brand + ' ' + 'del' + ' ' + this.year + ' di colore ' + this.color 
+  }
+    
+}
+// creo una nuova istanza
+const Fiat =  new Auto('Fiat', 2019 ,'blu', 4, 'petrol' )
+//loggo la istanza con il metodo per avere le info
+console.log(Fiat.informazioni());
